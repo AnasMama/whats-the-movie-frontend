ba29688fa0bdae2020b2a0a983e49d831179b7ed
MAMA Anas <anas.mama@hotmail.com>
2022-02-02 11:51:16 +0100

    ajout du router de base

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function NotFound() {
+  return (
+    <NotFoundContainer>
+      <h1>Oups !</h1>
+      <p>
+        Il semblerait que la page recherchée n'existe pas.
+      </p>
+      <p>
+        Mais vous pouvez :
+      </p>
+      <div>
+        <Link to="/dashboard">
+          <button type="button">Accéder à vos Business Plan</button>
+        </Link>
+        <Link to="/">
+          <button type="button">Revenir à la page d'accueil</button>
+        </Link>
+      </div>
+    </NotFoundContainer>
+  );
+}
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+
+export default NotFound;
